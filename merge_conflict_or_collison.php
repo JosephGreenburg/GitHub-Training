7d8310654c78dfae914c86bb82801eff441825ac
JosephGreenburg <joegreenburg@peakroi.com>
2023-01-31 17:16:26 -0600

    Commit for merge conflict 2.

--- merge_conflict_or_collison.php
+++ merge_conflict_or_collison.php
@@ -6,7 +6,12 @@ Create Branch - git checkout -b featrue-branch
 Change this file, commmit with messages, push to working branch. 
 Create pull request
 Merge pull request
+
+Make a second branch that edits the same line
+ACP, Pull Request and merge. 
+
 Create a collison/conflict on purpose and go through the steps
 */
 
 echo "Goodbye Repo";
+echo "This is on the same line and should create a conflict";
