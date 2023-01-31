c7a570d0dd38b29dbe121c487f75e98c09615c65
JosephGreenburg <joegreenburg@peakroi.com>
2023-01-31 17:36:16 -0600

    commiting after head reset

---
+++ merge_conflict_or_collison.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Make changes Add, Commit & Push to Master
+Create Branch - git checkout -b featrue-branch
+Change this file, commmit with messages, push to working branch. 
+Create pull request
+Merge pull request
+Create a collison/conflict on purpose and go through the steps
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+echo "Goodbye Repo";
+
+echo "This is on the same line and should create a conflict";
+
+echo "Conflicts can come in all shapes and sizes";
